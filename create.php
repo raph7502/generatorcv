f066cee20848f7bb96ff9584161996dd8c2c03b7
Raphael Bin <raphael.bin@epitech.eu>
2026-01-14 15:47:35 +0100

    first commit

---
+++ create.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> 
+<html lang="fr"> 
+    <head> 
+        <meta charset="UTF-8" /> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> <title>Générateur de CV</title> 
+         <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    </head> 
+       <body>
+         <h1>Générateur de CV</h1>
+         <p>creer ton cv</p>
+        </body> 
+ </html>
